1e6eb8cec13f455b0697553e54e981e588c14308
Daniel Walldorf <entekmedia@gmail.com>
2015-07-06 17:48:24 +0200

    Application entry point

---
+++ src/main/java/dwalldorf/elSampleroRandomo/Application.java
@@ -0,0 +1,12 @@
+package dwalldorf.elSampleroRandomo;
+
+/**
+ * Application entry point
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+
+}
