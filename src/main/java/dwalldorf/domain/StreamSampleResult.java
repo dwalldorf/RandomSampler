aabc7bad41ed7ca96c74980717f9911f8cbc7631
Daniel Walldorf <entekmedia@gmail.com>
2015-07-09 08:18:07 +0200

    StreamSampleResult domain obj

---
+++ src/main/java/dwalldorf/domain/StreamSampleResult.java
@@ -0,0 +1,26 @@
+package dwalldorf.domain;
+
+import java.util.List;
+
+/**
+ * Domain object to wrap a list of {@link StreamSample}s and store additional information about the stream.
+ */
+public class StreamSampleResult {
+
+  private List<StreamSample> sampleResult;
+
+  private long processedBytes;
+
+  public StreamSampleResult(List<StreamSample> sampleResult, long processedBytes) {
+    this.sampleResult = sampleResult;
+    this.processedBytes = processedBytes;
+  }
+
+  public List<StreamSample> getSamples() {
+    return sampleResult;
+  }
+
+  public long getProcessedBytes() {
+    return processedBytes;
+  }
+}
